fc019afe1c4184780a2a54ab198117d3b5f22413
Aguss <agustinampo80@gmail.com>
2025-03-30 21:40:01 -0300

    Full Stack Despliegue Back.10

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -6,7 +6,7 @@ import { createChannelController } from '../controllers/channel.controller.js'
 const authRoute = express.Router()
 
 authRoute.post('/register', registerUsers)
-authRoute.get('/verify-email', verifyEmailController)
+authRoute.get('/', verifyEmailController)
 authRoute.post('/login', loginController)
 authRoute.post('/reset-password', resetPasswordController)
 authRoute.put('/rewrite-password', rewritePasswordController)
